1e47ce7ea9a141682c8c0dd8fdfd968eb6940063
Kelly Chang <u2kelly619@gmail.com>
2022-05-25 09:42:11 +0800

    1. Add bootstrap, vue-axios 2. LoginView: connect to node.js

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap"
 
-createApp(App).use(store).use(router).mount('#app')
+// createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(VueAxios, axios).mount('#app')
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -4,15 +4,20 @@
   <div class="form-login container-fluid py-5">
     <div class="row justify-content-center">
       <div class="col-8 col-md-4">
-        <main class="form-signin">
-          <form>
+          <form class="needs-validation" novalidate>
             <div class="form-floating mb-3">
-              <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" required>
-              <label for="floatingInput">信箱</label>
+              <input type="email" class="form-control" id="email"  name="email" placeholder="name@example.com" v-model="email" required>
+              <label for="email" class="form-label">信箱</label>
+              <div class="invalid-feedback">
+                  請輸入合法的電子郵件地址。
+              </div>
             </div>
             <div class="form-floating mb-3">
-              <input type="password" class="form-control" id="floatingPassword" placeholder="Password" required>
-              <label for="floatingPassword">密碼</label>
+              <input type="password" class="form-control" id="password" name="password" minlength="8"  maxlength="12" placeholder="Password" v-model="password" required>
+              <label for="password" class="form-label">密碼</label>
+              <div class="invalid-feedback">
+                    請輸入 8 ~ 12 位數的密碼。
+              </div>
             </div>
         
             <div class="checkbox mb-3">
@@ -20,9 +25,8 @@
                 <input type="checkbox" value="remember-me"> Remember me
               </label>
             </div>
-            <button class="w-100 btn btn-lg btn-primary" type="submit">登入</button>
+            <button class="w-100 btn btn-lg btn-primary" @click.prevent="doLogin">登入</button>
           </form>
-        </main>
       </div>
     </div>
   </div>
@@ -30,13 +34,50 @@
 </template>
 
 <script>
-$(document).ready(function(){
-    const $navbar = $('#navbar');
-   
-    $(document).on('scroll',function(){
-            $navbar.offset().top > 80 ? 
-                $navbar.addClass('shadow') : $navbar.removeClass('shadow');
-        })
-  });
+export default {
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        doLogin() {
+            console.log(this.axios)
+            const submitForm = {
+                email: this.email,
+                password: this.password
+            }
+            this.axios.post('/login', submitForm).then((response) => {
+                console.log(response)
+                this.$router.push('/')
+            })
+        }
+    }
+}
 
+
+
+//bootstrap表單驗證
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+      // (function () {
+      //   'use strict'
+
+      //   // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      //   var forms = document.querySelectorAll('.needs-validation')
+      //   console.log(forms);
+
+      //   // Loop over them and prevent submission
+      //   Array.prototype.slice.call(forms)
+      //     .forEach(function (form) {
+      //       form.addEventListener('submit', function (event) {
+      //         if (!form.checkValidity()) {
+      //           event.preventDefault()
+      //           event.stopPropagation()
+      //         }
+
+      //         form.classList.add('was-validated')
+      //       }, false)
+      //     })
+      // })()
 </script>
